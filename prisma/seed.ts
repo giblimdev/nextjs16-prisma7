328c0d9127042c808ee8f2a943b76d1f818cfdad
giblimbev <giblimdev@google.com>
2026-01-14 01:55:40 +0100

    first commit

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -11,36 +11,7 @@ const prisma = new PrismaClient({
 });
 
 const userData: Prisma.UserCreateInput[] = [
-  {
-    name: "Alice",
-    email: "alice@prisma.io",
-    posts: {
-      create: [
-        {
-          title: "Join the Prisma Discord",
-          content: "https://pris.ly/discord",
-          published: true,
-        },
-        {
-          title: "Prisma on YouTube",
-          content: "https://pris.ly/youtube",
-        },
-      ],
-    },
-  },
-  {
-    name: "Bob",
-    email: "bob@prisma.io",
-    posts: {
-      create: [
-        {
-          title: "Follow Prisma on Twitter",
-          content: "https://www.twitter.com/prisma",
-          published: true,
-        },
-      ],
-    },
-  },
+  //ici un objet corectement typer
 ];
 
 export async function main() {
